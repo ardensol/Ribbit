8a0d60a955057a51d99cd81810d34ff2867a0605
carlos <csolorio05@gmail.com>
2015-10-24 01:08:33 -0400

    added parse

---
+++ app/src/main/java/com/carlos/rabbit/RabbitApplication.java
@@ -0,0 +1,24 @@
+package com.carlos.rabbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by carlos on 10/24/15.
+ */
+public class RabbitApplication extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "7c1Oqllek83roYkkbaNZDYrEDOzDNRgsUoMfQY2B", "SycrRnweBuU3NxLu0oCbZuIyIgnmIcvpHXeka1EX");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
