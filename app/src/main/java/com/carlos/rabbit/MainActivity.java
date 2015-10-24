4778e87aae25725b715bc762ee265e9b37745774
carlos <csolorio05@gmail.com>
2015-10-24 00:31:48 -0400

    added login

--- app/src/main/java/com/carlos/rabbit/MainActivity.java
+++ app/src/main/java/com/carlos/rabbit/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * {@link FragmentPagerAdapter} git remote add origin git@github.com:ardensol/ribbit.gitderivative, which will keep every
      * loaded fragment in memory. If this becomes too memory intensive, it
      * may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
