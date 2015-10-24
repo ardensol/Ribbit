6f8fb8498c72a6c46fb2a369f07679123fc2955d
carlos <csolorio05@gmail.com>
2015-10-24 12:57:44 -0400

    added get list

--- app/src/main/java/com/carlos/rabbit/EditFriendsActivity.java
+++ app/src/main/java/com/carlos/rabbit/EditFriendsActivity.java
@@ -1,30 +1,74 @@
 package com.carlos.rabbit;
 
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
 
-public class EditFriendsActivity extends AppCompatActivity {
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
+import java.util.List;
+
+public class EditFriendsActivity extends ListActivity {
+
+    public static final String TAG = EditFriendsActivity.class.getSimpleName();
+    protected List<ParseUser> mUsers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_friends);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.orderByAscending(ParseConstants.KEY_USERNAME);
+        query.setLimit(1000);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                setProgressBarIndeterminateVisibility(false);
+                if (e == null ) {
+                    mUsers = users;
+                    String[] usernames = new String[mUsers.size()];
+                    int i = 0;
+                    for (ParseUser user : mUsers ) {
+                        usernames[i] = user.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            EditFriendsActivity.this,
+                            android.R.layout.simple_list_item_checked,
+                            usernames);
+                    setListAdapter(adapter);
+                }
+                else {
+                    Log.e(TAG, e.getMessage());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
+                    builder.setMessage(e.getMessage())
+                            .setTitle(R.string.error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/carlos/rabbit/ParseConstants.java
@@ -0,0 +1,14 @@
+package com.carlos.rabbit;
+
+/**
+ * Created by carlos on 10/24/15.
+ */
+public final class ParseConstants {
+    //class names
+
+    //field names
+
+    public static final String KEY_USERNAME = "username";
+
+
+}
