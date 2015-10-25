fe43be68fd1592b2e4a2daf2d17d5ae641507a64
carlos <csolorio05@gmail.com>
2015-10-25 11:42:01 -0400

    added Parse Retrieve Inbox functionality

--- app/src/main/java/com/carlos/rabbit/MainActivity.java
+++ app/src/main/java/com/carlos/rabbit/MainActivity.java
@@ -25,9 +25,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.io.File;
@@ -36,6 +41,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -57,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
     protected DialogInterface.OnClickListener mDialogListener =
             new DialogInterface.OnClickListener() {
+
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     switch(which) {
@@ -212,6 +219,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
